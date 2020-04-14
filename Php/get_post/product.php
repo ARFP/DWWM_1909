e47ab0d40e308cb829c1f9e9b1788fd4f3428c5f
MDevoldere <mdevoldere@devoldere.net>
2020-04-14 14:20:07 +0200

    Start

---
+++ Php/get_post/product.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PHP POST</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1>Bonjour </h1>
+
+    <form>
+        <h2>Ajouter un produit</h2>
+        <fieldset>
+            <label for="product">Produit</label>
+            <input type="text" id="product" name="product_name" value="" placeholder="Nom du produit" required>
+        </fieldset>
+
+        <fieldset>
+            <label for="price">Prix</label>
+            <input type="number" min="1" step="0.5" id="price" name="product_price" value="" placeholder="Prix du produit" required>
+        </fieldset>
+
+        <button type="submit">Valider</button>
+
+    </form>
+
+    <section>
+        <h2>Liste des produits</h2>
+    </section>
+</body>
+</html>
