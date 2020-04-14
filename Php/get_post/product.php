0d86713e86a95ddd7237c649302f2caa4d24edbe
MDevoldere <mdevoldere@devoldere.net>
2020-04-14 16:44:18 +0200

    Partie 1

--- Php/get_post/product.php
+++ Php/get_post/product.php
@@ -1,17 +1,29 @@
 <?php
+session_start();
+// $nom = $_GET['nom'] ?? null;
+// null, '', [], 0, false
+if (empty($_GET['nom'])) {
+    header('location: ./index.html');
+    exit();
+}
+
+$nom = basename($_GET['nom']);
 
 ?>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>PHP POST</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
-    <h1>Bonjour </h1>
+    <h1>Bonjour <?= $nom; ?></h1>
 
     <form action="result.php" method="post">
+        <input type="hidden" name="user_name" value="<?= $nom; ?>">
         <h2>Ajouter un produit</h2>
         <fieldset>
             <label for="product">Produit</label>
@@ -29,6 +41,14 @@
 
     <section>
         <h2>Liste des produits</h2>
+        <?php
+        //var_dump($_SESSION);
+
+        foreach ($_SESSION['products'] as $product) {
+            echo ('<p>'. $product['nom'] . ' à ' .  $product['prix'] . '€ ajouté par ' . $product['utilisateur']. '</p>');
+        }
+        ?>
     </section>
 </body>
+
 </html>
--- Php/get_post/result.php
+++ Php/get_post/result.php
@@ -1,3 +1,33 @@
 <?php
+session_start();
+
+if(empty($_POST['product_name']) || empty($_POST['product_price'])  || empty($_POST['user_name']) ) {
+    header('location: ./product.php');
+    exit;
+}
+
+$user_name = $_POST['user_name'];
+$product_name = $_POST['product_name'];
+$product_price = intval($_POST['product_price']);
+
+
+if($product_price < 1) {
+    header('location: ./product.php');
+    exit;
+}
+
+// echo $product_name . ' ' . $product_price;
+
+$newproduct = array(
+    'nom' => $product_name,
+    'prix' => $product_price,
+    'utilisateur' => $user_name
+);
+
+$_SESSION['products'][] = $newproduct;
+
+$_SESSION['products_count'] = count($_SESSION['products']);
+
+header('location: ./product.php?nom='.$user_name.'');
 
 ?>
