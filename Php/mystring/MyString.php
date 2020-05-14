574feb3c4d9479b1ea3bcce593b754bcb3f0d522
MDevoldere <mdevoldere@devoldere.net>
2020-05-14 16:36:23 +0200

    Example

--- Php/mystring/MyString.php
+++ Php/mystring/MyString.php
@@ -12,11 +12,66 @@
 class MyString
 {
 
+    protected $str;
 
-    public function __construct()
+    protected $length;
+
+
+    public function __construct(string $input)
     {
-        
+        $this->str = $input;
+        $this->length = strlen($this->str);
     }
+
+    public function __toString()
+    {
+        return 'La chaine '. $this->str. ' est de longueur '. $this->length;
+    }
+
+
+    public function charAt(int $position)
+    {
+        return $this->str[$position] ?? '';
+    }
+
+    public function indexOf(string $char)
+    {
+        if(strlen($char) === 1) {
+            return strpos($this->str, $char);
+        }
+
+        return -1;
+    }
+
+    public function substring(int $debut, int $fin)
+    {
+        return substr($this->str, $debut, $fin);
+    }
+
+    public function split(string $separateur)
+    {
+        return explode($separateur, $this->str);
+    }
+
+    public function toLowerCase()
+    {
+        // return strtolower($this->str);
+        return mb_convert_case($this->str, MB_CASE_LOWER);
+    }
+
+    public function toUpperCase()
+    {
+        //return strtoupper($this->str);
+        return mb_convert_case($this->str, MB_CASE_UPPER);
+    }
+
+    public function toTitle()
+    {
+        //return strtoupper($this->str);
+        return mb_convert_case($this->str, MB_CASE_TITLE);
+    }
+
+    
 
 
 
--- Php/mystring/index.php
+++ Php/mystring/index.php
@@ -5,3 +5,22 @@
  */
 require 'MyString.php';
 
+$monObjet = new MyString("HeLLo éverybody, welCome to PHP 7 !");
+
+echo $monObjet."\n";
+
+echo $monObjet->charAt(17);
+echo "\n";
+echo $monObjet->charAt(1337);
+echo "\n";
+echo $monObjet->indexOf('y');
+echo "\n";
+echo $monObjet->substring(3, 17);
+echo "\n";
+var_dump($monObjet->split(','));
+echo "\n";
+echo $monObjet->toLowerCase();
+echo "\n";
+echo $monObjet->toUpperCase();
+echo "\n";
+echo $monObjet->toTitle();
